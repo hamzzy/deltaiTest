2378f4bd1e3ec047bd5209bb305dd7e25d357f34
ibrahim hamzat <hamat.ibrahim3@gmail.com>
2020-06-18 15:58:56 +0100

    solution improved

--- Untitled.py
+++ Untitled.py
@@ -3,18 +3,18 @@
 
 # In[33]:
 
+from functools import lru_cache
 
-def factorial(n): 
+@lru_cache(maxsize=None)
+def factorial(n):
       
-    # single line to find factorial 
+    # single line to find factorial
     return 1 if (n==1 or n<=0) else n * factorial(n - 1);
-
 
 # In[35]:
 
-
+print("The factorial is"+ factorial(5))
 get_ipython().run_line_magic('timeit', 'factorial(5)')
-
 
 # In[ ]:
 
